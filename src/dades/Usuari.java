d98bc44e5f863c3b5bbb78412af89e4da6d9fb15
Julia <julia.sabate.lopez@gmail.com>
2025-11-30 18:48:49 +0100

    .

--- src/dades/Usuari.java
+++ src/dades/Usuari.java
@@ -1,4 +1,34 @@
 package dades;
 
-public class Usuari {
+public abstract class Usuari {
+
+    private String alias;
+    private String correuInici;
+
+    public Usuari(String alias, String correuInici) {
+        this.alias = alias;
+        this.correuInici = correuInici;
+    }
+
+    public String getAlias() {
+        return this.alias;
+    }
+
+    public String getCorreuInici() {
+        return this.correuInici;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuari Base (Alias: " + alias + ", Correu Inicial: " + correuInici + ")";
+    }
+
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Usuari altreUsuari = (Usuari) obj;
+        return this.alias.equals(altreUsuari.alias);
+    }
 }
