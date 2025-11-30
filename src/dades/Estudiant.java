99f48310614381aa69c10fb54bab8fe96b75add2
Julia <julia.sabate.lopez@gmail.com>
2025-11-30 17:57:58 +0100

    .

--- src/dades/Estudiant.java
+++ src/dades/Estudiant.java
@@ -1,4 +1,23 @@
 package dades;
 
-public class Estudiant {
+public class Estudiant extends Usuari{
+    private String ensenyament;
+    private int anyInici;
+    public Estudiant(String alias, String correuInici, String ensenyament, int anyInici) {
+        super(alias, correuInici);
+        this.ensenyament = ensenyament;
+        this.anyInici = anyInici;
+    }
+
+    public String getEnsenyament() {
+        return this.ensenyament;
+    }
+
+    public int getAnyInici() {
+        return this.anyInici;
+    }
+
+    public void setAnyInici(int anyInici) {
+        this.anyInici = anyInici;
+    }
 }
