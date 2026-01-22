839d7f61c016da128b91be109a7970c2e1e8a0cd
julio <151668478+juliaasabatee@users.noreply.github.com>
2026-01-22 13:01:33 +0100

    Update Activitat.java

--- src/Dades/Activitat.java
+++ src/Dades/Activitat.java
@@ -3,24 +3,46 @@ package Dades;
 import java.time.LocalDate;
 
 public abstract class Activitat {
+
     protected String nom;
     protected String[] collectius;
     protected LocalDate dataIniciInscripcio;
     protected LocalDate dataFiInscripcio;
-    protected String responsable; // responsable de l'activitat (p. ex. "Pau")
+    protected String responsable;
     protected int capacitat;
     protected int inscrits;
 
-    public Activitat(String nom, String[] collectius, LocalDate dataIniciInscripcio, LocalDate dataFiInscripcio) {
+    public Activitat(String nom, String[] collectius,
+                     LocalDate dataIniciInscripcio,
+                     LocalDate dataFiInscripcio,
+                     int capacitat) {
         this.nom = nom;
         this.collectius = collectius;
         this.dataIniciInscripcio = dataIniciInscripcio;
         this.dataFiInscripcio = dataFiInscripcio;
+        this.capacitat = capacitat;
+        this.inscrits = 0;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public int getCapacitat() {
+        return capacitat;
+    }
+
+    public int getInscrits() {
+        return inscrits;
     }
 
     public boolean estaEnPeriode(LocalDate data) {
         return (data.isEqual(dataIniciInscripcio) || data.isAfter(dataIniciInscripcio)) &&
                (data.isEqual(dataFiInscripcio) || data.isBefore(dataFiInscripcio));
+    }
+
+    public boolean esEnTerminiInscripcio(LocalDate data) {
+        return estaEnPeriode(data);
     }
 
     public boolean haAcabat(LocalDate data) {
@@ -31,10 +53,6 @@ public abstract class Activitat {
         return inscrits < capacitat;
     }
 
-    public boolean esEnTerminiInscripcio(LocalDate data) {
-        return !haAcabat(data);
-    }
-
     public void inscriure() {
         inscrits++;
     }
@@ -43,64 +61,10 @@ public abstract class Activitat {
         if (inscrits > 0) inscrits--;
     }
 
-    public String getNom() {
-        return nom;
-    }
+    public abstract boolean hiHaClasseAvui(LocalDate data);
 
     @Override
     public String toString() {
         return nom + " (" + inscrits + "/" + capacitat + ")";
-    }
-
-    public abstract boolean hiHaClasseAvui(LocalDate data);
-    }
-
-    public Activitat() {
-    }
-
-
-
-    public Activitat(String nom) {
-        this.nom = nom;
-    }
-
-    public String getNom() {
-        return nom;
-    }
-
-    public void setNom(String nom) {
-        this.nom = nom;
-    }
-
-    public String[] getCollectius() {
-        return collectius;
-    }
-
-    public void setCollectius(String[] collectius) {
-        this.collectius = collectius;
-    }
-
-    public LocalDate getDataIniciInscripcio() {
-        return dataIniciInscripcio;
-    }
-
-    public void setDataIniciInscripcio(LocalDate dataIniciInscripcio) {
-        this.dataIniciInscripcio = dataIniciInscripcio;
-    }
-
-    public LocalDate getDataFiInscripcio() {
-        return dataFiInscripcio;
-    }
-
-    public void setDataFiInscripcio(LocalDate dataFiInscripcio) {
-        this.dataFiInscripcio = dataFiInscripcio;
-    }
-
-    public String getResponsable() {
-        return responsable;
-    }
-
-    public void setResponsable(String responsable) {
-        this.responsable = responsable;
     }
 }
