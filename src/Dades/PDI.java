3e5bec7232672c662fa9582fd1e6dc89fc0a2fe0
julio <151668478+juliaasabatee@users.noreply.github.com>
2026-01-22 13:03:38 +0100

    Modify PDI class constructor and clean up methods
    Updated PDI constructor to include department and campus parameters. Removed unused getter and setter methods for department and campus.

--- src/Dades/PDI.java
+++ src/Dades/PDI.java
@@ -1,32 +1,19 @@
 package Dades;
 
 public class PDI extends Usuari {
-    
+
     private String departament;
     private String campus;
 
-    public PDI(String alies, String correu) {
-        super(alies, correu);
+    public PDI(String alias, String correu,
+               String departament, String campus) {
+        super(alias, correu);
         this.departament = departament;
         this.campus = campus;
     }
+
+    @Override
     public String getCollectiu() {
-        return "PDI"; 
-    }
-
-    public String getDepartament() {
-        return departament;
-    }
-
-    public void setDepartament(String departament) {
-        this.departament = departament;
-    }
-
-    public String getCampus() {
-        return campus;
-    }
-
-    public void setCampus(String campus) {
-        this.campus = campus;
+        return "PDI";
     }
 }
