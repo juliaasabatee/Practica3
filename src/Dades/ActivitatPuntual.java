57a1a4e0b9b77a8f6ad550e6dd59d4ffa382462b
julio <151668478+juliaasabatee@users.noreply.github.com>
2026-01-22 13:02:23 +0100

    Refactor ActivitatPuntual class by removing limitPlaces

--- src/Dades/ActivitatPuntual.java
+++ src/Dades/ActivitatPuntual.java
@@ -8,90 +8,30 @@ public class ActivitatPuntual extends Activitat {
     private LocalDate data;
     private LocalTime horaInici;
     private String ciutat;
-    private int limitPlaces;
     private double preu;
-    private double duracioHores = 1.0; // duración en horas por defecto
-
-    public ActivitatPuntual() {
-        super();
-    }
+    private double duracioHores;
 
     public ActivitatPuntual(String nom,
+                            String[] collectius,
+                            LocalDate dataIniciInscripcio,
+                            LocalDate dataFiInscripcio,
+                            int capacitat,
                             LocalDate data,
                             LocalTime horaInici,
                             String ciutat,
-                            int limitPlaces,
-                            double preu) {
-        super(nom);
-        this.data = data;
-        this.horaInici = horaInici;
-        this.ciutat = ciutat;
-        this.limitPlaces = limitPlaces;
-        this.preu = preu;
-    }
-
-    public ActivitatPuntual(String nom,
-                            LocalDate data,
-                            LocalTime horaInici,
-                            String ciutat,
-                            int limitPlaces,
                             double preu,
-                            double duracioHores, int capacitat) {
-        this(nom, data, horaInici, ciutat, limitPlaces, preu);
-        this.duracioHores = duracioHores;
-        this.capacitat = capacitat;
-        this.inscrits = 0;
-    }
+                            double duracioHores) {
 
-    public LocalDate getData() {
-        return data;
-    }
-
-    public void setData(LocalDate data) {
+        super(nom, collectius, dataIniciInscripcio, dataFiInscripcio, capacitat);
         this.data = data;
-    }
-
-    public LocalTime getHoraInici() {
-        return horaInici;
-    }
-
-    public void setHoraInici(LocalTime horaInici) {
         this.horaInici = horaInici;
-    }
-
-    public String getCiutat() {
-        return ciutat;
-    }
-
-    public void setCiutat(String ciutat) {
         this.ciutat = ciutat;
-    }
-
-    public int getLimitPlaces() {
-        return limitPlaces;
-    }
-
-    public void setLimitPlaces(int limitPlaces) {
-        this.limitPlaces = limitPlaces;
-    }
-
-    public double getPreu() {
-        return preu;
-    }
-
-    public void setPreu(double preu) {
         this.preu = preu;
-    }
-
-    public double getDuracioHores() {
-        return duracioHores;
-    }
-
-    public void setDuracioHores(double duracioHores) {
         this.duracioHores = duracioHores;
     }
+
     @Override
-    public boolean hiHaClasseAvui(LocalDate data) {
-        return data.equals(dataClasse);
+    public boolean hiHaClasseAvui(LocalDate dataAvui) {
+        return this.data.equals(dataAvui);
     }
 }
